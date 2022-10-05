3b3f6875a794ca1484b35af8183aa68cde88c093
Gustas <juskevicius.gustas@gmail.com>
2022-10-05 16:00:50 +0300

    Added some UI changes

---
+++ Pantry/Pantry/pages/AddProductPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Pantry.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.CommunityToolkit.PlatformConfiguration.iOSSpecific;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pantry.pages
+{
+    public partial class AddProductPage : Popup
+    {
+        public AddProductPage()
+        {
+            InitializeComponent();
+        }
+        private void BtnProductAddToList(object sender, EventArgs e)
+        {
+            DataHandler.AddProduct(new Product() { productName = ProductName.Text, expiryDate = ExpiryDate.Date, productColor = SelectColor.SetColor(ExpiryDate.Date), daysLeft = SelectColor.DisplayDaysLeft(ExpiryDate.Date)});
+        }
+    }
+}
