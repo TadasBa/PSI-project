ac16524e00e1ec99ad872916dd2ad80de0e40e51
Erika <erikaklimait@gmail.com>
2022-10-11 23:15:04 +0300

    Adding product type picker functionality, filter by expiry date and product type, image source by product type

---
+++ Pantry/Pantry/models/ProductTypeHandler.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using Pantry.enums;
+using Pantry.models;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace Pantry.models
+{
+    public static class ProductTypeHandler
+
+    {
+        public static List<string> values { get; set; }
+
+        static ProductTypeHandler()
+        {
+            values = new List<string>();
+            foreach (ProductType pt in Enum.GetValues(typeof(ProductType)))
+            {
+                String value = pt.ToDescriptionString();
+                values.Add(value);
+
+            }
+
+            values.Sort();
+        }
+
+        public static void setImageSource(Product product)
+        {
+            switch (product.type)
+            {
+                case ProductType.BAKED_GOODS:
+                    product.imageSource = "";
+                    break;
+
+                case ProductType.BERRIES:
+                    product.imageSource = "";
+                    break;
+
+                case ProductType.DAIRY:
+                    product.imageSource = "";
+                    break;
+
+                case ProductType.DRINKS:
+                    product.imageSource = "";
+                    break;
+
+                case ProductType.EGGS:
+                    product.imageSource = "";
+                    break;
+
+                case ProductType.FRUITS:
+                    product.imageSource = "";
+                    break;
+
+                case ProductType.GRAINS:
+                    product.imageSource = "";
+                    break;
+
+                case ProductType.LEGUMES:
+                    product.imageSource = "";
+                    break;
+
+                case ProductType.MEAT:
+                    product.imageSource = "";
+                    break;
+
+                case ProductType.NUTS_AND_SEEDS:
+                    product.imageSource = "";
+                    break;
+
+                case ProductType.SEAFOOD:
+                    product.imageSource = "";
+                    break;
+
+                case ProductType.VEGETABLES:
+                    product.imageSource = "";
+                    break;
+
+            }
+
+        }
+
+    }
+
+}
