659252e9860a4d919fae8fdf3cc95e7ae7db2722
misterej <eitvis.stul@gmail.com>
2022-11-01 13:16:39 +0200

    created backend API

---
+++ BackendAPI/Controllers/ProductController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BackendAPI.Data;
+using BackendAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductDBContext _dbContext;
+
+        public ProductController(ProductDBContext context)
+        {
+            _dbContext = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _dbContext.Products.ToListAsync();
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _dbContext.Products.FindAsync(id);
+            return product == null ? NotFound() : Ok(product);
+        }
+
+        [HttpGet("UsrID")]
+        public async Task<IActionResult> GetByUsrId(int usrid)
+        {
+            var product = await _dbContext.Products.W;
+            return product == null ? NotFound() : Ok(product);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> CreateUser(Product product)
+        {
+            await _dbContext.Products.AddAsync(product);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new {id = product.Id}, product);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Update(int id, Product product)
+        {
+            if(id != product.Id) return BadRequest();
+
+            _dbContext.Entry(product).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var productToDelete = await _dbContext.Products.FindAsync(id);
+            if(productToDelete == null) return NotFound();
+
+            _dbContext.Products.Remove(productToDelete);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ BackendAPI/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
