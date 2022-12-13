3b37218b197b7db948bf0bb46fa59580337f555f
misterej <eitvis.stul@gmail.com>
2022-12-13 11:21:09 +0200

    integration tests for Recepie/Product/ and auth login

--- BackendAPI.Test/BasicTests.cs
+++ BackendAPI.Test/BasicTests.cs
@@ -2,6 +2,8 @@
 using Xunit;
 using BackendAPI;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text;
 
 namespace BackendAPI.Test
 {
@@ -16,7 +18,8 @@ namespace BackendAPI.Test
 
         [Theory]
         [InlineData("/api/Product")]
-        public async Task HttpGetProduct(string url)
+        [InlineData("/api/Recepie")]
+        public async Task HttpGet(string url)
         {
             // Arrange
             var client = _factory.CreateClient();
@@ -28,6 +31,55 @@ namespace BackendAPI.Test
             response.EnsureSuccessStatusCode();
             Assert.Equal("application/json; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
+        }
+
+        [Theory]
+        [InlineData("/api/Product/id?id=0")]
+        [InlineData("/api/Recepie/id?id=0")]
+        public async Task HttpGet404(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            Assert.Equal("NotFound", response.StatusCode.ToString());
+        }
+
+        [Theory]
+        [InlineData("/auth/login")]
+        public async Task HttpAuthLogin(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            string content = "{\"id\": 0,\"name\": \"string\",\"password\": \"string\",\"email\": \"string\"}";
+            StringContent c = new StringContent(content, UTF32Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(url, c);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+
+        [Theory]
+        [InlineData("/auth/login")]
+        public async Task HttpAuthLogin404(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            string content = "{\"id\": 0,\"name\": \"string\",\"password\": \"N/A\",\"email\": \"N/A\"}";
+            StringContent c = new StringContent(content, UTF32Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(url, c);
+
+            // Assert
+            Assert.Equal("NotFound", response.StatusCode.ToString());
         }
     }
 }
