44a0bfb76fbc18a891fa0caa55d6d2bd5ce75b2c
misterej <eitvis.stul@gmail.com>
2022-12-13 10:55:49 +0200

    FirstIntegrationTest / works!

---
+++ BackendAPI.Test/BasicTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+using BackendAPI;
+using System.Threading.Tasks;
+
+namespace BackendAPI.Test
+{
+    public class BasicTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public BasicTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/api/Product")]
+        public async Task HttpGetProduct(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
